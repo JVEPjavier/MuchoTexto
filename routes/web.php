91b3b13cfa5ab3319bbedef35ddf09e9682b87ec
Javier <javi.varela.perez@gmail.com>
2024-04-28 20:30:54 -0400

    Migrando al repositorio

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,21 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Models\Usuario;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', HomeController::class);
+
+Route::get('prueba', function() {
+    $usuario = new Usuario;
+
+    $usuario->NombreUsuario = "segundo usuario";
+    $usuario->CorreoElectronico = "equisdee@equisde";
+    $usuario->Contraseña = "equisde123";
+    $usuario->FechaRegistro = date('Y-m-d H:i:s');
+    $usuario->RolUsuario = 2;
+
+    $usuario->save();
+
+    return $usuario;
 });
